8268346deb861b17638d218873ea29fa29aa2560
Travis Exline <t.exline22@gmail.com>
2019-08-13 14:41:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let solution = []
+  for ( let i = 0; i < names.length; i++ ) {
+    solution.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+  }
+  return solution
+}
+
+function countdown(number) {
+  let count = number
+  while (count >= 0) {
+    console.log(count--)
+  }
+}
