9141a2ef47d627a3efe7a5339b69383613ad8ade
M Banks <montensia1@outlook.com>
2019-08-15 01:34:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(str_arr, event){
+  let result = [];
+  for(let i = 0; i < str_arr.length; i++){
+    result.push(`Thank you, ${str_arr[i]}, for the wonderful surprise gift!`);
+  }
+  return result;
+}
+
+function countdown(num){
+
+  while(num >= 0 ){
+    console.log(num);
+    num--;
+  }
+}
+
+
